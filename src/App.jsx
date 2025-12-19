f3ac379bd16db4f1d891bdb5a7da3b8e7c435564
Shreyponkiya <Shrey@tecoreng.com>
2025-12-19 11:00:55 +0530

    revert last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Dashboard from './pages/Dashboard';
-import DailyLog from './pages/DailyLog';
-import Growth from './pages/Growth';
-import Tasks from './pages/Tasks';
-import Notes from './pages/Notes';
-import Login from './pages/Login'; // Import Login
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Dashboard from "./pages/Dashboard";
+import DailyLog from "./pages/DailyLog";
+import Growth from "./pages/Growth";
+import Tasks from "./pages/Tasks";
+import Notes from "./pages/Notes";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        {/* Public Route */}
-        <Route path="/login" element={<Login />} />
+    <Routes>
+      {/* Public Route */}
+      <Route path="/login" element={<Login />} />
 
-        {/* Protected Routes */}
-        <Route element={<Layout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/daily-log" element={<DailyLog />} />
-          <Route path="/growth" element={<Growth />} />
-          <Route path="/tasks" element={<Tasks />} />
-          <Route path="/notes" element={<Notes />} />
-          <Route path="/" element={<Dashboard />} />
-        </Route>
-      </Routes>
-    </Router>
+      {/* Protected Routes */}
+      <Route element={<Layout />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/daily-log" element={<DailyLog />} />
+        <Route path="/growth" element={<Growth />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/notes" element={<Notes />} />
+      </Route>
+    </Routes>
   );
 };
 
